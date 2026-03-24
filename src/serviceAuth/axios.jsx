41147232fd4a8d8837636c2791b77ed39aae5632
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2026-03-24 12:38:34 +0530

    as

--- src/serviceAuth/axios.jsx
+++ src/serviceAuth/axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 const api = axios.create({
-  baseURL: "https://vmm9pgj8-5000.inc1.devtunnels.ms/api",
+  baseURL: "http://localhost:5000/api",
+  // baseURL: "https://vmm9pgj8-5000.inc1.devtunnels.ms/api",
 });
 
 // 🔐 Attach token
